bc233aade63359511f72d8cc79b9b0503b82960f
Chris Gardner <ctgardner89@gmail.com>
2016-09-27 11:07:01 -0400

    tweaking

--- script.js
+++ script.js
@@ -8,26 +8,61 @@ app.config(["$stateProvider", function($stateProvider){
   })
   .state("show", {
     url: "/legislators/:id",
-    templateUrl: "./show.html",
-    controller: "legislatorShow"
+    views: {
+      "main": {
+        templateUrl: "./show.html"
+      },
+      "partial": {
+        // templateUrl: "./comment.html"
+      }
+    },
   })
   .state("mainPage",{
     url: "/",
     templateUrl: "./mainPage.html"
   })
+  .state("bills", {
+    url: "/bills",
+    templateUrl: "./billIndex.html",
+    controller: "billIndex"
+  })
+  .state("billShow",{
+    url: "/bills/:id",
+    templateUrl: "./billShow.html",
+    controller: "billShow"
+  })
 }])
 
 app.factory("factry",["$resource", function($resource){
-  return $resource('https://thawing-island-32605.herokuapp.com/legislators/:id', {}, {
-    update: {method: 'PUT'}
-  })
+  return $resource('https://thawing-island-32605.herokuapp.com/legislators/:id', {},{
+    update: {
+      method: 'PUT'
+    }
+  });
+}])
+
+app.factory("billFactry", function($resource){
+  return $resource('https://thawing-island-32605.herokuapp.com/bills/:id')
+})
+
+app.controller("billIndex", ["$scope", "billFactry", function($scope, billFactry){
+  $scope.bills = billFactry.query();
+
 }])
 
 app.controller("legis",["$scope","factry", function($scope, factry){
   $scope.legislators = factry.query();
 }])
 
-app.controller("legislatorShow", ["$scope", "factry", "$stateParams", function($scope, factry, $stateParams){
+app.controller("billShow", ["$scope", "billFactry", "$stateParams", function($scope, billFactry, $stateParams){
+  $scope.bill = billFactry.get({id: $stateParams.id}, function(res){
+   $scope.sponsor = angular.fromJson(res.sponsor)
+   $scope.pdf = angular.fromJson(res.last_version)
+    console.log($scope.pdf)
+  })
+}])
+
+app.controller("legislatorShow", ["$scope", "factry", "$stateParams", "$state", function($scope, factry, $stateParams, $state){
   $scope.votes = [];
   $scope.legislator = factry.get({id: $stateParams.id}, function(res){
     angular.forEach(res.votes, function(vote){
@@ -39,12 +74,41 @@ app.controller("legislatorShow", ["$scope", "factry", "$stateParams", function($
             newObj.vote = blob[res.legislator.bio_id];
             newObj.bill = vote[1];
             newObj.bill_title = bill[0];
+            newObj.bill_id = bill[2]
+            // console.log(newObj.bill_id)
             $scope.votes.push(newObj);
           }
         })
       }
     });
   });
+  // $scope.putComment = function($state){
+  //   console.log(this.comment);
+  //   console.log(this.poster);
+  //   $scope.newComment = this.comment;
+  //   $scope.poster = this.poster
+  //   $scope.legislator = factry.get({id: $stateParams.id},function(res){
+  //     var obj = {};
+  //     obj.comment = $scope.newComment;
+  //     obj.posted_by = $scope.poster;
+  //     console.log(res.comments)
+  //     res.comments.push(obj)
+  //     res.$update()
+  //     res.$save()
+  //     console.log(res.comments)
+  //   });
+  //     // $scope.legislator.comments
+  //     // res.comments.save(this.comment);
+  //   this.comment = '';
+  // };
+  $scope.switch = function(){
+      $scope.voteHistory = true;
+      $scope.Hascomments = false
+  };
+  $scope.comments = function(){
+      $scope.voteHistory = false;
+      $scope.Hascomments = true
+  }
 
 }])
 
